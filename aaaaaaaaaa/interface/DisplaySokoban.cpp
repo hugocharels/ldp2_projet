fde520bb80e5bceb5ecbfde66c81c31c96df53b7
hugocharels <hugocharels@hotmail.com>
2022-11-25 11:39:54 +0100

    add a switch case on draw cell

--- aaaaaaaaaa/interface/DisplaySokoban.cpp
+++ aaaaaaaaaa/interface/DisplaySokoban.cpp
@@ -4,8 +4,20 @@
 
 
 void DisplaySokoban::drawACell(Point display_pos, Cell* cell){
-	
-
+	switch(cell->getType()) {
+		case EMPTY:
+			break;
+		case WALL;
+			break;
+		case TP:
+			break;
+		case TARGET:
+			break;
+		case PLAYER:
+			break;
+		case BOX:
+			break;
+	}
 }
 
 void DisplaySokoban::draw(){
