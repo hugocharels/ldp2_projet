a533a91fdc9bf2cff8f6e8c026fce733da3f7df6
hugocharels <hugocharels@hotmail.com>
2022-12-13 17:13:07 +0100

    lock resize

--- aaaaaaaaaa/interface/MainWindow.h
+++ aaaaaaaaaa/interface/MainWindow.h
@@ -28,7 +28,6 @@ public:
 
 	MainWindow(Sokoban* sokoban):Fl_Double_Window(500, 200, windowWidth, windowHeight, "Sokoban"), sokoban_model{sokoban} {
 		Fl::add_timeout(1.0 / refreshPerSecond, Timer_CB, this);
-		resizable(this);
 	}
 
 	void draw() {
