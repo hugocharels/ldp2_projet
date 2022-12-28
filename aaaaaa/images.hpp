9087080414060f795d679f83c96da7fe8dc18259
Hugo <93338995+hugocharels@users.noreply.github.com>
2022-12-28 11:06:10 +0000

    j'avais oublié les levels

---
+++ aaaaaa/images.hpp
@@ -0,0 +1,23 @@
+const char* IM_WALL       = "images/wall.png";
+const char* FLOOR         = "images/floor.png";
+const char* IM_PLAYER     = "images/playerew.png";
+
+const char* NONE_BOX      = "images/nonebox.png";
+const char* RED_BOX       = "images/redbox.png";
+const char* ORANGE_BOX    = "images/orangebox.png";
+const char* YELLOW_BOX    = "images/yellowbox.png";
+const char* GREEN_BOX     = "images/greenbox.png";
+const char* BLUE_BOX      = "images/bluebox.png";
+const char* PURPLE_BOX    = "images/purplebox.png";
+
+const char* NONE_TARGET   = "images/nonetarget.png";
+const char* RED_TARGET    = "images/redtarget.png";
+const char* ORANGE_TARGET = "images/orangetarget.png";
+const char* YELLOW_TARGET = "images/yellowtarget.png";
+const char* GREEN_TARGET  = "images/greentarget.png";
+const char* BLUE_TARGET   = "images/bluetarget.png";
+const char* PURPLE_TARGET = "images/purpletarget.png";
+
+const char* GREEN_TP      = "images/greentp.png";
+const char* PINK_TP       = "images/pinktp.png";
+const char* PURPLE_TP     = "images/purpletp.png";
