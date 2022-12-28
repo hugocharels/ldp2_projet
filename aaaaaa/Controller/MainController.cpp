6b5d6258cd5375f7d93990a8ffc05dd2b2ad404c
Hugo <93338995+hugocharels@users.noreply.github.com>
2022-12-28 12:23:31 +0000

    oups

--- aaaaaa/Controller/MainController.cpp
+++ aaaaaa/Controller/MainController.cpp
@@ -6,10 +6,11 @@
 
 void MainController::keyPressed(int key_code, GAME_STATE state) {
 	std::cout << key_code << std::endl;
+	std::cout << (int)state << std::endl;
 }
 
 
 void MainController::mouseClick(Point mouse_loc, GAME_STATE state) {
 	std::cout << mouse_loc.x << "/" << mouse_loc.y << std::endl;
-    std::cout << (int)state << std::endl;
+	std::cout << (int)state << std::endl;
 }
