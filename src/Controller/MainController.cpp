b1d1262f893206af27232301fdbee67cc524ca1c
Hugo <93338995+hugocharels@users.noreply.github.com>
2022-12-29 17:28:09 +0000

    avec commentaire de debug

--- src/Controller/MainController.cpp
+++ src/Controller/MainController.cpp
@@ -10,16 +10,20 @@ void MainController::keyPressed(int key_code, GAME_STATE &state) {
 			state = GAME_STATE::PLAY;
 			break;
 
-		case GAME_STATE::WON:
-		case GAME_STATE::LOST:
-			menu.keyPressed(key_code);
-			state = menu.getNewState();
-			break;
-			
 		case GAME_STATE::PLAY:
 			board.keyPressed(key_code);
 			menu.keyPressed(key_code);
+			std::cout << " key board " << (int)state;
 			state = board.getNewState();
+			std::cout << " --> " << (int)state << std::endl;
+			break;
+
+		case GAME_STATE::WON:
+		case GAME_STATE::LOST:
+			menu.keyPressed(key_code);
+			std::cout << " key menu " << (int)state;
+			state = menu.getNewState();
+			std::cout << " --> " << (int)state << std::endl;
 			break;
 
 		case GAME_STATE::EDITOR:
@@ -41,13 +45,17 @@ void MainController::mouseClick(Point mouse_loc, GAME_STATE &state) {
 		case GAME_STATE::PLAY:
 			menu.mouseClick(mouse_loc);
 			board.mouseClick(mouse_loc);
+			std::cout << " mouse board " << (int)state;
 			state = board.getNewState();
+			std::cout << " --> " << (int)state << std::endl;
 			break;
 
 		case GAME_STATE::WON:
 		case GAME_STATE::LOST:
 			menu.mouseClick(mouse_loc);
+			std::cout << " mouse menu " << (int)state;
 			state = menu.getNewState();
+			std::cout << " --> " << (int)state << std::endl;
 			break;
 			
 		case GAME_STATE::EDITOR:
