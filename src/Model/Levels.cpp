a2cae0488763cb51d1e4dbbf5e78b938fea4672f
Hugo <93338995+hugocharels@users.noreply.github.com>
2023-01-04 01:40:09 +0000

    ok la j'ai vraiment fini

--- src/Model/Levels.cpp
+++ src/Model/Levels.cpp
@@ -79,6 +79,14 @@ void Levels::saveBoard(Board &board, int step_limit) {
 	std::string line = "";
 	for (int i = 0; i < map->getCols(); i++) {
 		for (int j = 0; j < map->getRows(); j++) {
+
+			for ( const auto& box : *board.getTouBoxDeg() ) {
+				if ( box.getPos() == Point{i, j} ) {
+					line += ColorToNum(box.getColor());
+					continue;
+				}
+			}
+
 			if (map->at(i, j)->getType() == TARGET) {
 				line += ColorToLetter(dynamic_cast<Target*>(map->at(i, j).get())->getColor());
 			}
