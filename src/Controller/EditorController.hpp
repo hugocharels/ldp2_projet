a8616106fcc00951fc71b1a874ab4e188b83ea15
Hugo <93338995+hugocharels@users.noreply.github.com>
2023-01-08 12:11:39 +0000

    bye comment code + truc qui sert a rien

--- src/Controller/EditorController.hpp
+++ src/Controller/EditorController.hpp
@@ -9,15 +9,11 @@
 class EditorController: public AbstractController {
 	Editor* model;
 	GAME_STATE state;
-	int current_select = 0;
-
-	void setCurrentSelect(int new_current) { current_select=new_current;}
 
 public:
 
 	void keyPressed(int key_code) override;
 	void mouseClick(Point mous_loc) override;
-	//void clickSelect(Point mouse_loc);
 
 	void setModel(Editor* m) { this->model = m; }
 	
